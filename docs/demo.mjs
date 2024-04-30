28abf3d1b4217e4fa46f1a9077feb0e4d57ccf8d
Manuel Simoni <msimoni@gmail.com>
2024-04-30 21:20:01 +0200

    simplify-event

--- docs/demo.mjs
+++ docs/demo.mjs
@@ -1,18 +1,17 @@
 import { run, suspend, resume } from "../index.mjs";
 import { sleep } from "../sleep.mjs";
-import { EventManager } from "../event.mjs";
+import { get_next_event, inject_event } from "../event.mjs";
 
-function* ball(x, y, xdelta, ydelta)
+function* ball(x, y)
 {
     while (true)
     {
+        console.log(x, y);
         yield* sleep(2000);
     }
 }
 
-const event_manager = new EventManager();
-
-function* main_loop()
+function* main()
 {
     // Spawn some pre-existing balls
     yield* run(ball(100, 200));
@@ -20,11 +19,11 @@ function* main_loop()
     yield* run(ball(200, 800));
     // Wait for click events, spawn a new ball on each click
     while (true) {
-        const event = yield* event_manager.get_next_event();
-        yield* run(ball, event.pageX, event.pageY);
+        const event = yield* get_next_event();
+        yield* run(ball(event.pageX, event.pageY));
     }
 }
 
-window.onload = () => run(main_loop()).next();
+window.onload = () => run(main()).next();
 
-window.onclick = (event) => event_manager.inject_event(event).next();
+window.onclick = (event) => inject_event(event).next();
--- event.mjs
+++ event.mjs
@@ -6,109 +6,48 @@
 import { suspend, resume } from "./index.mjs";
 
 /*
- * There should be a single event manager and a single main generator
- * reading events from it in an app.
+ * Events arriving from event handlers (e.g. `window.onclick`) should
+ * be piped into the `inject_event` method.
  *
- * Events arriving from event handlers (e.g. `window.onclick`) should be
- * piped into the `inject_event` method.
- *
- * The main generator can block on events by calling `get_next_event`.
+ * A generator can block on events by calling `get_next_event`.
  *
  * This currently doesn't do any queueing - if an event arrives when
- * the main generator isn't waiting for an event (which can happen
- * when it is doing another async task, like a network request), the
- * event is simply dropped.
- *
- * This is implemented as a class so that users can subclass it, and
- * override behavior for what should happen when a concurrent
- * `get_next_event` happens, or when an event is dropped.
+ * no generator isn't waiting for an event (which can happen when it
+ * is doing another async task, like a network request), the event is
+ * simply dropped.
  */
-export class EventManager
+
+let waiting_generator = null;
+
+/*
+ * Suspend the calling generator until an event arrives.
+ *
+ * The generator gets saved in the waiting_generator property.
+ */
+export function* get_next_event()
 {
-    constructor()
-    {
-        /*
-         * Internally, we save a reference to the generator blocking
-         * on an event.
-         *
-         * When an event arrives, we resume the generator with the
-         * event as the result of the call to get_next_event().
-         */
-        this.waiting_generator = null;
-    }
-
-    /*
-     * Suspend the current generator until an event arrives.
-     *
-     * The generator gets saved in the waiting_generator property.
-     */
-    *get_next_event()
-    {
-        const self = this;
-        return yield* suspend(function* (generator) {
-            if (self.waiting_generator === null) {
-                /*
-                 * Set the waiting generator to the generator that
-                 * performed the call to get_next_event().
-                 */
-                self.waiting_generator = generator;
-            } else {
-                /*
-                 * A generator is already waiting for an event.  This
-                 * is a bug in the application - only one generator
-                 * should be reading events.
-                 */
-                self.on_concurrent_get_next_event();
-            }
-        });
-    }
-
-    /*
-     * Add an event coming from an external event handler.
-     *
-     * Wake up the waiting generator and return the event from its
-     * call to get_next_event().
-     */
-    *inject_event(event)
-    {
-        if (this.waiting_generator !== null) {
-            /*
-             * Resume the waiting generator, and set the
-             * waiting_generator property to null, so we don't
-             * accidentally resume it twice.
-             */
-            const generator = this.waiting_generator;
-            this.waiting_generator = null;
-            yield* resume(generator, function* () { return event; });
+    return yield* suspend(function* (generator) {
+        if (waiting_generator === null) {
+            waiting_generator = generator;
         } else {
-            /*
-             * No generator is waiting for an event, drop it.
-             */
-            this.on_dropped_event(event);
+            throw "Concurrent attempt at get_next_event";
         }
-    }
+    });
+}
 
-    /*
-     * This method gets called when more than one generator attempts
-     * to read events.  You can override it.
-     *
-     * This indicates a bug in the application and should always throw.
-     */
-    on_concurrent_get_next_event()
-    {
-        throw "Concurrent attempt at get_next_event";
-    }
-
-    /*
-     * This method gets called when an event arrives, but no generator
-     * is waiting for an event.  You can override it.
-     *
-     * This can happen for example when the event-handling generator
-     * is currently performing some other async task like a network
-     * request.
-     */
-    on_dropped_event(event)
-    {
-        console.log("Dropped event", event)
+/*
+ * Add an event coming from an external event handler.
+ *
+ * Wake up the waiting generator and return the event from its
+ * call to get_next_event().
+ */
+export function *inject_event(event)
+{
+    if (waiting_generator !== null) {
+        const generator = waiting_generator;
+        waiting_generator = null;
+        yield* resume(generator, function* () { return event; });
+    } else {
+        console.log("Dropped event", event);
     }
 }
