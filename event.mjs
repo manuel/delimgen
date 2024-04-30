17fb58634ebbb73fa8ba2d239bcfbda79a6d74c5
Manuel Simoni <msimoni@gmail.com>
2024-04-30 19:00:49 +0200

    wip

--- event.mjs
+++ event.mjs
@@ -9,15 +9,19 @@ import { suspend, resume } from "./index.mjs";
  * There should be a single event manager and a single main generator
  * reading events from it in an app.
  *
- * Events arriving from event handlers (e.g. window.onclick) should be
- * piped into the inject_event() method.
+ * Events arriving from event handlers (e.g. `window.onclick`) should be
+ * piped into the `inject_event` method.
  *
- * The main generator can block on events by calling get_next_event().
+ * The main generator can block on events by calling `get_next_event`.
  *
  * This currently doesn't do any queueing - if an event arrives when
  * the main generator isn't waiting for an event (which can happen
  * when it is doing another async task, like a network request), the
  * event is simply dropped.
+ *
+ * This is implemented as a class so that users can subclass it, and
+ * override behavior for what should happen when a concurrent
+ * `get_next_event` happens, or when an event is dropped.
  */
 export class EventManager
 {
@@ -30,20 +34,24 @@ export class EventManager
          * When an event arrives, we resume the generator with the
          * event as the result of the call to get_next_event().
          */
-        this.saved_generator = null;
+        this.waiting_generator = null;
     }
 
     /*
      * Suspend the current generator until an event arrives.
      *
-     * The generator gets saved in the saved_generator property.
+     * The generator gets saved in the waiting_generator property.
      */
     *get_next_event()
     {
         const self = this;
         return yield* suspend(function* (generator) {
-            if (self.saved_generator === null) {
-                self.saved_generator = generator;
+            if (self.waiting_generator === null) {
+                /*
+                 * Set the waiting generator to the generator that
+                 * performed the call to get_next_event().
+                 */
+                self.waiting_generator = generator;
             } else {
                 /*
                  * A generator is already waiting for an event.  This
@@ -63,9 +71,14 @@ export class EventManager
      */
     *inject_event(event)
     {
-        if (this.saved_generator !== null) {
-            const generator = this.saved_generator;
-            this.saved_generator = null;
+        if (this.waiting_generator !== null) {
+            /*
+             * Resume the waiting generator, and set the
+             * waiting_generator property to null, so we don't
+             * accidentally resume it twice.
+             */
+            const generator = this.waiting_generator;
+            this.waiting_generator = null;
             yield* resume(generator, function* () { return event; });
         } else {
             /*
