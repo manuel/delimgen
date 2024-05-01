d987b130e0064ae2b6d489b14c086f407cef30f0
Manuel Simoni <msimoni@gmail.com>
2024-05-01 10:32:21 +0200

    rect

--- docs/demo.mjs
+++ docs/demo.mjs
@@ -2,26 +2,44 @@ import { run, suspend, resume } from "../index.mjs";
 import { sleep } from "../sleep.mjs";
 import { get_next_event, inject_event } from "../event.mjs";
 
-function* ball(x, y)
+function* rect(x, y)
 {
+    const rect = document.createElement("div");
+    rect.classList.add("rect");
+    rect.style.left = x + "px";
+    rect.style.top = y + "px";
+    document.body.appendChild(rect);
+    const xdelta = rand(0, 5);
+    const ydelta = rand(0, 5);
     while (true)
     {
-        console.log(x, y);
-        yield* sleep(2000);
+        x += xdelta;
+        y += ydelta;
+        rect.style.left = x + "px";
+        rect.style.top = y + "px";
+        yield* sleep(50);
     }
 }
 
 function* main()
 {
-    // Spawn some pre-existing balls
-    yield* run(ball(100, 200));
-    yield* run(ball(400, 600));
-    yield* run(ball(200, 800));
-    // Wait for click events, spawn a new ball on each click
+    /*
+     * Spawn some pre-existing rects.
+     */
+    yield* run(rect(100, 200));
+    yield* run(rect(200, 300));
+    yield* run(rect(300, 400));
+    /*
+     * Wait for click events, spawn a new rect on each click.
+     */
     while (true) {
         const event = yield* get_next_event();
-        yield* run(ball(event.pageX, event.pageY));
+        yield* run(rect(event.pageX, event.pageY));
     }
+}
+
+function rand(min, max) {
+    return Math.random() * (max - min) + min;
 }
 
 window.onload = () => run(main()).next();
